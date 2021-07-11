88f187550605264927bb60d410b3a59edc64451f
Ayisha Mirfath <ayisha.mirfath@gmail.com>
2021-07-11 12:53:31 +0530

    end of section 6

---
+++ client/src/app/messags/messags.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messags',
+  templateUrl: './messags.component.html',
+  styleUrls: ['./messags.component.css']
+})
+export class MessagsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
